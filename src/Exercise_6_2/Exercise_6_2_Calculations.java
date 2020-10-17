81824a7fa22e85f780b31ebd87eaa6b941000a4a
KRL1991 <kimx4706@edu.easj.dk>
2020-10-17 13:24:07 +0200

    added 6_2

--- src/Exercise_6_2/Exercise_6_2_Calculations.java
+++ src/Exercise_6_2/Exercise_6_2_Calculations.java
@@ -1,4 +1,19 @@
 package Exercise_6_2;
 
 public class Exercise_6_2_Calculations {
-}
+
+    public static int sumDigits( long n ) {
+
+        int sum = 0;
+        do {
+            sum += n % 10;
+
+        } while ((n = n / 10) != 0);
+
+        return sum;
+
+    }
+
+
+    }
+
--- src/Exercise_6_2/Exercise_6_2_Main.java
+++ src/Exercise_6_2/Exercise_6_2_Main.java
@@ -1,4 +1,23 @@
 package Exercise_6_2;
 
+import java.util.Scanner;
+
+import static Exercise_6_2.Exercise_6_2_Calculations.sumDigits;
+
 public class Exercise_6_2_Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter an integer to sum");
+
+        long n = input.nextInt();
+
+        System.out.println("The sum is " + Exercise_6_2_Calculations.sumDigits(n));
+
+
+
+
+    }
 }
